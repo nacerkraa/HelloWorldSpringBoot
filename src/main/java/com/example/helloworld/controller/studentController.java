4a687f5e02f989c2978cf6bc281ce9205ec1d07e
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-10-26 17:13:48 +0100

    access data from templates

--- src/main/java/com/example/helloworld/controller/studentController.java
+++ src/main/java/com/example/helloworld/controller/studentController.java
@@ -1,16 +1,20 @@
 package com.example.helloworld.controller;
 
+import com.example.helloworld.model.Student;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
 
 
 @Controller
 public class studentController {
 
-
-    @GetMapping Mapping("/home")
-    public String index(){
+    @GetMapping ("/home")
+    public String index(Model model){
+        Student student = new Student(1L,"meriam","meriam@gmail.com", LocalDate.of(2000,12,03),25);
+        model.addAttribute("Student",student);
         return "home";
     }
 }
